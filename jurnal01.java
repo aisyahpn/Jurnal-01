6d639b3aa3abf8dc6d64a9214ebf21d76ef56b66
aisyahpn <aisyahpn05@gmail.com>
2023-09-27 09:27:59 +0700

    Jurnal 01

---
+++ jurnal01.java
@@ -0,0 +1,7 @@
+public class jurnal01 {
+    public static void main (String[] args) {
+        System.out.println("Nama: Aisyah Putri Nuraini");
+        System.out.println("NIM: 607062300033");
+        System.out.println("Kelas: D3IF-47-04");
+    }
+}
